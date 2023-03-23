ab114a4cfba006bd5997630aac4d4bb677af156b
izhukov <izhukov@btlab.ru>
2023-03-23 15:40:27 +0300

    v4.3.5 - beta

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,8 @@ import FileUploader from "./components/FileUploader";
 const App = () => {
     return (
         <div>
-            <FileUploader url={"http://localhost:5000"}/>
+            {/*<FileUploader url={"http://localhost:5000"}/>*/}
+            <FileUploader url={"http://185.253.7.239:5000"}/>
         </div>
     );
 };
